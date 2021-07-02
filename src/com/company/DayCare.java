4d871859923c9c9cebdfd2d17718befcdc00c520
Tfauves <tfauvelle@gmail.com>
2021-07-02 12:38:03 -0400

    refactored Daycare methods

--- src/com/company/DayCare.java
+++ src/com/company/DayCare.java
@@ -2,15 +2,25 @@ package com.company;
 import java.util.*;
 
 public class DayCare {
+//
+//    List<Animal> animals = new ArrayList<>();
 
-    List<Animal> animals = new ArrayList<>();
-
-    public void addAnimal(Animal animal) {
-        animals.add(animal);
+    public static void addAnimal(Animal animal, List<Animal> animalList) {
+        animalList.add(animal);
     }
 
-//    for (Animal animal : animals) {
-//
-//    }
+    public static void displayAnimals(List<Animal> animals) {
+        for (Animal animal : animals) {
 
+            System.out.println(animal.name + " has " + animal.legs + " legs, and weighs " + animal.weight);
+
+
+        }
+
+    }
+
+    public static void removeAnimal(List<Animal> animals, String name) {
+        animals.removeIf(animal -> animal.name.equals(name));
+
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,42 +1,69 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 
-    Animal frog = new Animal(
-            4,
-            "small",
-            false,
-            "Raul",
-            2.0
-    );
+//    Animal frog = new Animal(
+//            4,
+//            "small",
+//            false,
+//            "Raul",
+//            2.0
+//    );
+//
+//    Cat cat = new Cat(
+//            4,
+//            "medium",
+//            true,
+//            "Henry",
+//            12.0,
+//            "calico",
+//            false,
+//            2
+//    );
+//
+//    Dog dog = new Dog(
+//            4,
+//            "Large",
+//            true,
+//            "Jasper",
+//            65.0,
+//            "Golden Retiever",
+//            "med",
+//            "#13"
+//    );
 
-    Cat cat = new Cat(
-            4,
-            "medium",
-            true,
-            "Henry",
-            12.0,
-            "calico",
-            false,
-            2
-    );
+        List<Animal> animals = new ArrayList<>();
 
-    Dog dog = new Dog(
-            4,
-            "Large",
-            true,
-            "Jasper",
-            65.0,
-            "Golden Retiever",
-            "med",
-            "#13"
-    );
+        DayCare.addAnimal(
+                new Animal(4, "small",false,"Raul",2.0),
+                animals
+        );
 
-    frog.speak();
-    cat.speak();
-    dog.speak();
+        DayCare.addAnimal(
+                new Cat(4, "medium", true, "Henry", 12.0, "calico", false, 2),
+                animals
+
+        );
+
+        DayCare.addAnimal(
+                new Dog(4,"large", true, "Jasper", 65.0, "Golden Retiever", "med", "#13"),
+                animals
+        );
+
+        DayCare.displayAnimals(animals);
+        DayCare.removeAnimal(animals, "Henry");
+        DayCare.displayAnimals(animals);
+
+
+
+//    frog.speak();
+//    cat.speak();
+//    dog.speak();
     }
 
 
